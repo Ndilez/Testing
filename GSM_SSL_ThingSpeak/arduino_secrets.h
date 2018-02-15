43680c166674804b476ed97621b28d16cd1c2dbe
simonmags <simongithub@gmail.com>
2018-02-15 14:44:43 +1030

    Publish_Initial_Example
    This is the initial sketch where I've added all the content for an untested example on how to do a read and write operation to a Thingspeak Channel which needs to be added into the Arduino Secrets file.

---
+++ GSM_SSL_ThingSpeak/arduino_secrets.h
@@ -0,0 +1,9 @@
+#define SECRET_PINNUMBER     ""
+#define SECRET_GPRS_APN      "internet" // replace your GPRS APN
+#define SECRET_GPRS_LOGIN    ""    // replace with your GPRS login
+#define SECRET_GPRS_PASSWORD "" // replace with your GPRS password
+
+// Thingspeak Settings
+#define secretwriteapikey  "123456789ABC"
+#define secretreadapikey  "123456789XYZ"
+#define secretchannelid   "123456"
